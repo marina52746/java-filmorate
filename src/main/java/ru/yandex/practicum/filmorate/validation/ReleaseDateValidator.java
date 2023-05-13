3e80b7c7bf41e11038140cda0fae8b32c3a7c840
Marina <marina52746@yandex.ru>
2023-05-13 22:01:01 +0300

    checkstyle^ braces

--- src/main/java/ru/yandex/practicum/filmorate/validation/ReleaseDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/ReleaseDateValidator.java
@@ -3,8 +3,7 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
 
-public class ReleaseDateValidator implements ConstraintValidator<ReleaseDateValidation, LocalDate>
-{
+public class ReleaseDateValidator implements ConstraintValidator<ReleaseDateValidation, LocalDate> {
     public boolean isValid(LocalDate date, ConstraintValidatorContext cxt) {
         LocalDate filmBirthday = LocalDate.of(1895, 12, 28);
         return (date != null) && (date.isAfter(filmBirthday) || date.isEqual(filmBirthday));
--- src/main/java/ru/yandex/practicum/filmorate/validation/WithoutWhitespacesValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/WithoutWhitespacesValidator.java
@@ -2,8 +2,7 @@ package ru.yandex.practicum.filmorate.validation;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-public class WithoutWhitespacesValidator implements ConstraintValidator<WithoutWhitespacesValidation, String>
-{
+public class WithoutWhitespacesValidator implements ConstraintValidator<WithoutWhitespacesValidation, String> {
     public boolean isValid(String login, ConstraintValidatorContext cxt) {
         return (login != null) && !login.contains(" ");
     }
